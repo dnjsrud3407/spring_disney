c89c97257a6a8ace68344580f7b7c6fc657ea90a
dnjsrud3407 <dnjsrud3407@naver.com>
2022-03-23 00:00:49 +0900

    회원 등록,조회 기능 추가

--- src/main/java/com/tistory/dnjsrud/disney/repository/UserRepository.java
+++ src/main/java/com/tistory/dnjsrud/disney/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.tistory.dnjsrud.disney.repository;
 import com.tistory.dnjsrud.disney.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByLoginId(String loginId);
 }
--- src/main/java/com/tistory/dnjsrud/disney/service/UserService.java
+++ src/main/java/com/tistory/dnjsrud/disney/service/UserService.java
@@ -6,8 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
@@ -18,11 +21,35 @@ public class UserService {
      * @param user
      * @return userId
      */
+    @Transactional
     public Long join(User user) {
+        // Validation 검증하는 부분은 강의 스프링 MVC 2편에 나와있다
+        // 회원 중복 체크
+        validateDuplicateMember(user);
         userRepository.save(user);
         return user.getId();
     }
 
+    private void validateDuplicateMember(User user) {
+        // 동시성 문제가 발생할 수 있다
+        List<User> findUsers = userRepository.findByLoginId(user.getLoginId());
+        if (!findUsers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 아이디입니다.");
+        }
+    }
 
+    // 회원 전체 조회
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * 회원정보 조회
+     * @param id
+     * @return User
+     */
+    public User findUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
 
 }
