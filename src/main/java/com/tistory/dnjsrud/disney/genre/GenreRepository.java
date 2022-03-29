7161fb3f7075f992d4d312d73af4e4a5ba70d22a
dnjsrud3407 <dnjsrud3407@naver.com>
2022-03-29 23:46:20 +0900

    repository optional로 변경

--- src/main/java/com/tistory/dnjsrud/disney/genre/GenreRepository.java
+++ src/main/java/com/tistory/dnjsrud/disney/genre/GenreRepository.java
@@ -4,7 +4,8 @@ import com.tistory.dnjsrud.disney.genre.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-    List<Genre> findByGenreName(String genreName);
+    Optional<Genre> findByGenreName(String genreName);
 }
--- src/main/java/com/tistory/dnjsrud/disney/user/UserRepository.java
+++ src/main/java/com/tistory/dnjsrud/disney/user/UserRepository.java
@@ -2,9 +2,19 @@ package com.tistory.dnjsrud.disney.user;
 
 import com.tistory.dnjsrud.disney.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
+
+    @Query("select u.loginId from User u where u.email = :email")
+    Optional<String> findLoginIdByEmail(@Param("email") String email);
 }
