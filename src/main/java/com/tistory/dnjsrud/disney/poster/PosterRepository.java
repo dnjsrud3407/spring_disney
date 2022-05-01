19fa9093df855b7066a2762f2ac745b720da879f
dnjsrud3407 <dnjsrud3407@naver.com>
2022-05-01 18:23:51 +0900

    PosterRepository 중복 구현 삭제

--- src/main/java/com/tistory/dnjsrud/disney/poster/PosterRepository.java
+++ src/main/java/com/tistory/dnjsrud/disney/poster/PosterRepository.java
@@ -3,5 +3,4 @@ package com.tistory.dnjsrud.disney.poster;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PosterRepository extends JpaRepository<Poster, Long> {
-    void deleteById(Long posterId);
 }
