6258c845645614969fe72c762487f2896c3ca66e
dnjsrud3407 <dnjsrud3407@naver.com>
2022-07-19 23:31:28 +0900

    movie 리뷰 있는 html, 리뷰 없는 html 분리

--- src/main/java/com/tistory/dnjsrud/disney/movie/MovieController.java
+++ src/main/java/com/tistory/dnjsrud/disney/movie/MovieController.java
@@ -6,11 +6,13 @@ import com.tistory.dnjsrud.disney.global.MyPage;
 import com.tistory.dnjsrud.disney.review.ReviewCreateForm;
 import com.tistory.dnjsrud.disney.review.ReviewDetailDto;
 import com.tistory.dnjsrud.disney.review.ReviewService;
+import com.tistory.dnjsrud.disney.user.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -63,19 +65,43 @@ public class MovieController {
 
     @GetMapping("/{movieId}")
     public String movieDetail(@PathVariable Long movieId, HttpServletRequest request, Model model,
-                              @PageableDefault(page = 0, size = 5) Pageable pageable) {
+                              @PageableDefault(page = 0, size = 5) Pageable pageable,
+                              @AuthenticationPrincipal SecurityUser securityUser) {
         MovieDetailDto movie = movieService.findMovieDetailDto(movieId);
         // 평점 소수점 둘째 자리까지 표기
         movie.setStar(Float.parseFloat(String.format("%.2f", movie.getStar())));
-        
-        Long userId = 66L;
+
         Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request);
         if(flashMap!=null) {
             pageable = (Pageable) flashMap.get("pageable");
         }
 
-        // 영화 리뷰 조회
-        Page<ReviewDetailDto> result = reviewService.findReviewDetailDtoListWithoutUser(pageable, movieId, userId);
+        Page<ReviewDetailDto> result;
+        if(securityUser != null) {
+            Long userId = securityUser.getId();
+            // 영화 리뷰 조회
+            result = reviewService.findReviewDetailDtoListWithoutUser(pageable, movieId, userId);
+
+            // 현재 유저가 작성한 리뷰
+            ReviewDetailDto myReview = reviewService.findReviewDetailDto(movieId, userId);
+            if(myReview != null) {
+                model.addAttribute("myReview", myReview);
+                model.addAttribute("totalCount", result.getTotalElements() + 1);
+
+                model.addAttribute("movie", movie);
+                model.addAttribute("reviewList", result.getContent());
+
+                MyPage page = new MyPage(result);
+                model.addAttribute("page", page);
+
+                return "movie/detailReview";
+            }
+        }
+
+        result = reviewService.findReviewDetailDtoList(pageable, movieId);
+
+        model.addAttribute("reviewCreateForm", new ReviewCreateForm());
+        model.addAttribute("totalCount", result.getTotalElements());
 
         model.addAttribute("movie", movie);
         model.addAttribute("reviewList", result.getContent());
@@ -83,15 +109,6 @@ public class MovieController {
         MyPage page = new MyPage(result);
         model.addAttribute("page", page);
 
-        // 현재 유저가 작성한 리뷰
-        ReviewDetailDto myReview = reviewService.findReviewDetailDto(movieId, userId);
-        if(myReview != null) {
-            model.addAttribute("myReview", myReview);
-            model.addAttribute("totalCount", result.getTotalElements() + 1);
-        } else {
-            model.addAttribute("reviewCreateForm", new ReviewCreateForm());
-            model.addAttribute("totalCount", result.getTotalElements());
-        }
-        return "movie/detail";
+        return "movie/detailReviewNone";
     }
 }
